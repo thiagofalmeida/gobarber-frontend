35125d3cbcff02c448ea18895a8e7b6ee0c61b8a
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-09-24 10:50:21 -0300

    Configurado estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => {
-  return <h1>Oi</h1>;
-};
+const App: React.FC = () => (
+  <>
+    <h1>Oi</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
