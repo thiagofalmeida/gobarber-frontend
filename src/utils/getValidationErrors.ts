ee44f06aed38ce613f25c373ea3201ea20442d8d
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-09-24 15:04:09 -0300

    Exibindo erros nos inputs

---
+++ src/utils/getValidationErrors.ts
@@ -0,0 +1,15 @@
+import { ValidationError } from 'yup';
+
+interface Errors {
+  [key: string]: string;
+}
+
+export default function getValidationErrors(err: ValidationError): Errors {
+  const validationErrors: Errors = {};
+
+  err.inner.forEach(error => {
+    validationErrors[err.path] = err.message;
+  });
+
+  return validationErrors;
+}
