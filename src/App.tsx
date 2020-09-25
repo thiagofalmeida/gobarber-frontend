3e720321d8db539a2bd206759be39484652e9290
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-09-25 10:48:54 -0300

    Logout da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -9,6 +9,7 @@ interface SignInCredencials {
 interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredencials): Promise<void>;
+  signOut(): void;
 }
 
 interface AuthState {
@@ -46,8 +47,15 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({ token, user });
   }, []);
 
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
