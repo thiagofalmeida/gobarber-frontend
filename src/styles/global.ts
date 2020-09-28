09598f12f47e405f820607b1a1fb6af97807ad6d
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-09-28 17:47:26 -0300

    Adicionado estilo global para background e color do input autocomplete

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -18,6 +18,12 @@ export default createGlobalStyle`
     font: 16px 'Roboto Slab', sans-serif;
   }
 
+  input:-webkit-autofill {
+    box-shadow: 0 0 0px 1000px #232129 inset;
+    -webkit-box-shadow: 0 0 0px 1000px #232129 inset;
+    -webkit-text-fill-color: #fff;
+  }
+
   button {
     cursor: pointer;
   }
