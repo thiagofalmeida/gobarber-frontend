5dc524cf08209c2267664563f1f4a2cf619058b3
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-09-28 20:34:50 -0300

    Animação ao carregar páginas de signin e signup

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,13 @@ import Routes from './routes';
 import AppProvider from './hooks';
 
 const App: React.FC = () => (
-  <>
+  <BrowserRouter>
     <AppProvider>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <Routes />
     </AppProvider>
 
     <GlobalStyle />
-  </>
+  </BrowserRouter>
 );
 
 export default App;
